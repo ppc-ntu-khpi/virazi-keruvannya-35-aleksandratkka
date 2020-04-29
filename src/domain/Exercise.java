5ad2a5c96fa8ca97c67c4ba91a2da59cefb9f1ad
aleksandratkka <63710963+aleksandratkka@users.noreply.github.com>
2020-04-29 13:45:46 +0300

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,27 @@
 package domain;
-
+/**
+ * class for calculate the second largest number 
+ * @author asus
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+public static int Calculate(int[] arr) {
+    int r = 0;
+
+    for (int i = 0; i < arr.length - 1; i++)
+
+            for (int j = 0; j < arr.length - i - 1; j++)
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    
+        for(int i=1; i<arr.length;i++){
+            if(arr[i]!= arr[i-1] )
+            { r = arr[i];
+              break;}
+            }
+
+    return r;
     }
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,13 @@
 package test;
-
-import domain.Exercise;
+import static domain.Exercise.Calculate;
+/**
+ * Test of a class that calculates the second largest number 
+ * @author asus
+ */
 
 public class TestResult {
-
-    public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
-    }
+  public static void main(String[] args) {
+     int[] array ={15, 68, 7, 3, 45, 6, 31};
+         System.out.println("Result: "+Calculate(array));
+}
 }
